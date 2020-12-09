c4fe1c27bfefbef7940a89c20635fc7284a25efd
Cameron Campbell <cameroncampbell@camerons-air.attlocal.net>
2020-12-09 12:09:21 -0500

    commiting before lunch

---
+++ client/src/components/layouts/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <h1>IronCRM</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
