ac40756f88dadc79fb1d4e8d2560c76cb4cc5456
Cameron Campbell <cameroncampbell@camerons-air.attlocal.net>
2020-12-09 14:08:03 -0500

    update

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -5,7 +5,18 @@ const Navbar = () => {
     <div className="navbar">
       <div className="logo">
         <h1>IronCRM</h1>
+        <p>Made by Cameron Campbell + Brandon Garrett</p>
       </div>
+      <ul>
+        <li>Hello...</li>
+        <span className="sm-hide">|</span>
+        <li>
+          <a href="#!">
+            <span className="sm-hide">Logout</span>
+            <i className="fas fa-sign-out-alt"></i>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
