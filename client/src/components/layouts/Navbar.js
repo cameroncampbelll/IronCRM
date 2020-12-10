521a3993356c65758eabdddfcb0717fe208cf1a1
brandongarrett <72109485+brandongarrett@users.noreply.github.com>
2020-12-10 11:15:32 -0600

    brandon navbar

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -3,11 +3,13 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="navbar">
+
       <div className="logo">
         <h1>IronCRM</h1>
+        <img src="../images/iron-crm-logo.png" alt='' className="iron-logo" />
         <p>Made by Cameron Campbell + Brandon Garrett</p>
       </div>
-      <ul>
+      <ul id='nav-buttons'>
         <li>Hello...</li>
         <span className="sm-hide">|</span>
         <li>
