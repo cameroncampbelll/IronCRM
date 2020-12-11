810b4d288b55699e179fbf5e3ccff3b5506e0c60
Cameron Campbell <cameroncampbell@camerons-air.attlocal.net>
2020-12-11 18:24:31 -0500

    working on lead form

--- client/src/components/layouts/LeadForm.js
+++ client/src/components/layouts/LeadForm.js
@@ -1,29 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
+
+// this is where i left off on friday
 
 const LeadForm = () => {
+  const [lead, setLead] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    contactType: "Non-contacted",
+    notes: "",
+  });
+
+  const { name, phone, email, contactType, notes } = lead;
+
+  const handleChange = (event) => {
+    setLead({
+      ...lead,
+      [event.target.name]: event.target.value,
+    });
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setLead({
+      name: "",
+      phone: "",
+      email: "",
+      contactType: "Non-contacted",
+      notes: "",
+    });
+  };
   return (
     <div className="invite-section">
       <h1 className="add-contact">Add Contact</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="add-field">
-          <input type="text" placeholder="Name" name="name" />
-          <input type="text" placeholder="Phone Number" name="Phone" />
-          <input type="text" placeholder="Email" name="email" />
-          <input type="text" placeholder="Notes" name="notes" />
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Phone Number"
+            name="phone"
+            value={phone}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Notes"
+            name="notes"
+            value={notes}
+            onChange={handleChange}
+          />
         </div>
         <h2 className="options-label">Contact Type</h2>
         <div className="options">
           <label className="container">
             Non-contacted
-            <input type="radio" name="lead" value="Non-contacted" />
+            <input
+              type="radio"
+              name="lead"
+              value="Non-contacted"
+              checked={contactType === "Non-contacted"}
+              onChange={handleChange}
+            />
           </label>
           <label className="container">
             Contacted
-            <input type="radio" name="lead" value="Contacted" />
+            <input
+              type="radio"
+              name="lead"
+              value="Contacted"
+              onChange={handleChange}
+            />
           </label>
           <label className="container">
             Sale-Pending
-            <input type="radio" name="lead" value="Sale-Pending" />
+            <input
+              type="radio"
+              name="lead"
+              value="Sale-Pending"
+              onChange={handleChange}
+            />
           </label>
         </div>
         <input type="submit" value="Add Contact" className="btn"></input>
