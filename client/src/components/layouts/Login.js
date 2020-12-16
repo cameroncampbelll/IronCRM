6de11944ff5cd0b38bd7bcf81c64c0c5f411312a
Cameron Campbell <cameroncampbell@camerons-air.attlocal.net>
2020-12-16 16:51:40 -0500

    authorization files setup

--- client/src/components/layouts/Login.js
+++ client/src/components/layouts/Login.js
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = user;
+
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    console.log({ email, password });
+  };
   return (
     <div className="login">
       <h1>Login</h1>
-      <form>
-        <input type="email" name="email" placeholder="email" />
-        <input type="password" name="password" placeholder="Password" />
+      <form onSubmit={submit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={handleChange}
+        />
         <input type="submit" value="Sign In" className="btn" />
       </form>
       <div className="question">
         <p>
-          Already have an account? <Link to="/login">Sign Up</Link>{" "}
+          Dont have an account? <Link to="/register">Sign Up</Link>{" "}
         </p>
       </div>
     </div>
--- client/src/components/layouts/Register.js
+++ client/src/components/layouts/Register.js
@@ -1,18 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+  const { name, email, password, password2 } = user;
+
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log("Passwords dont match");
+    } else {
+      console.log({ name, email, password });
+    }
+  };
+
   return (
     <div className="register">
       <h1>Sign Up</h1>
-      <form>
-        <input type="text" name="name" placeholder="name" />
-        <input type="email" name="email" placeholder="email" />
-        <input type="password" name="password" placeholder="Password" />
+      <form onSubmit={submit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="name"
+          value={name}
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          value={email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={handleChange}
+        />
         <input
           type="password"
           name="password2"
           placeholder="Confirm Password"
+          value={password2}
+          onChange={handleChange}
         />
         <input type="submit" value="Sign Up" className="btn" />
       </form>
