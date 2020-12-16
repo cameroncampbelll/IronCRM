f3a7035981d925cda4478038fb86b905872d5cb3
brandongarrett <72109485+brandongarrett@users.noreply.github.com>
2020-12-16 16:46:53 -0600

    css-pretty-good

--- client/src/components/layouts/Login.js
+++ client/src/components/layouts/Login.js
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <div className="login">
-      <h1>Login</h1>
+      <h1 className='login-title'>Login</h1>
       <form>
-        <input type="email" name="email" placeholder="email" />
-        <input type="password" name="password" placeholder="Password" />
+        <input type="email" name="email" placeholder="Email" className="add-comp" />
+        <input type="password" name="password" placeholder="Password" className="add-comp" />
         <input type="submit" value="Sign In" className="btn" />
       </form>
       <div className="question">
-        <p>
-          Already have an account? <Link to="/login">Sign Up</Link>{" "}
+        <p className='already'>
+          Already have an account? <Link to="/login" className='sign-up-btn' >Sign Up</Link>{" "}
         </p>
       </div>
     </div>
